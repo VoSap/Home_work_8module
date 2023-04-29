3b8e0522fd86760fc8014bacf231c02ce5926ee7
VoSap <130397170+VoSap@users.noreply.github.com>
2023-04-29 10:44:27 +0200

    Add files via upload
    Corrected

--- get_birthdays_per_week.py
+++ get_birthdays_per_week.py
@@ -2,22 +2,22 @@ from datetime import datetime, timedelta
 from collections import defaultdict
 
 users = [
-    {'name': 'Ivan', 'birthday': datetime(1990, 4, 28)},
-    {'name': 'Vova', 'birthday': datetime(1985, 4, 27)},
-    {'name': 'Oksana', 'birthday': datetime(1995, 5, 2)}
+    {'name': 'Ivan', 'birthday': datetime(1990, 5, 1)},
+    {'name': 'Vova', 'birthday': datetime(1985, 4, 30)},
+    {'name': 'Oksana', 'birthday': datetime(1995, 5, 2)},
+    {'name': 'Volo', 'birthday': datetime(1985, 5, 5)},
+    {'name': 'Vovik', 'birthday': datetime(1985, 5, 6)},
 ]
 
 
 def get_birthdays_per_week(users):
     today = datetime.now().date()
-    for user in users:
-        user['birthday'] = user['birthday'].replace(year=today.year)
     next_week = today + timedelta(days=7)
     weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     birthdays = defaultdict(list)
 
     for user in users:
-        birthday = user['birthday'].date()
+        birthday = user['birthday'].replace(year=today.year).date()
         if birthday >= today and birthday <= next_week:
             weekday = weekdays[birthday.weekday()]
             if weekday in ['Saturday', 'Sunday']:
